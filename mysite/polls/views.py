3c956222f47b126582408e307403b3cf2c1c331c
jinsungPark <dksk0101@gmail.com>
2021-11-10 15:30:54 +0900

    tutorial

---
+++ mysite/polls/views.py
@@ -0,0 +1,55 @@
+from django import http
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, response
+# Create your views here.
+from django.template import loader
+from .models import Question
+from django.http import Http404
+
+
+#1
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
+
+#2
+# def index(request):
+#     latest_question_list = Question.objects.order_by('=pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+# short rander
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+#404 error
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
+
+# short 404 error
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)  
+
+
